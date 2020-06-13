0ca405719e0f8afe702e1e7c5b3cd3fdef931b2c
DevLead <atmainaction@gmail.com>
2020-06-13 21:46:01 +0530

    video 3 files

---
+++ Exercise Files/04/finish/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+import Greet from './Greet'
+
+function App() {
+  return (
+    <div className="App">
+      <Greet />
+      <Greet />
+      <Greet />
+      <Greet />
+    </div>
+  );
+}
+
+export default App;
---
+++ Exercise Files/04/finish/src/Greet.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './greet.css'
+
+function Greet() {
+    return (
+    <div className="greet">Hello, DevLead</div>
+    )
+}
+
+export default Greet;
